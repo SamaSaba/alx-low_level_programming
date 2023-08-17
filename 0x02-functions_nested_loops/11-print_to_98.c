a1fdc5ed64779f35ac0a1e5a32d6f25d375db2d1
SamaSaba <samasabea@gmail.com>
2023-08-17 13:55:43 -0700

    removing space

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,12 +1,15 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_to_98 - unction that prints all natural numbers from n to 98
- */ 
+ * print_to_98 - function that prints all natural numbers from n to 98
+ * @n: number to start from
+ * Return: void
+ */
 
 void print_to_98(int n)
 {
 	int i;
+
 	if (n >= 0 && n <= 98)
 	{
 		for (i = n ; i <= 98 ; i++)
@@ -37,7 +40,7 @@ void print_to_98(int n)
 		}
 		printf("\n");
 		}
-	else if ( n == 98)
+	else if (n == 98)
 		printf("%d\n", n);
 }
 
